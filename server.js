be38614c8959959a48e05225dbe47b8636c79457
salaj07 <salaj9877@gmail.com>
2025-11-28 15:01:52 +0530

    asd

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const connectDb= require("./src/db/db")
 connectDb()
 
 const PORT=process.env.PORT
-app.listen(PORT,()=>{
-    console.log("server started");
+// app.listen(PORT,()=>{
+//     console.log("server started");
     
+// })
+app.use((req,res,next)=>{
+    next ()
 })
 
+module.exports=app

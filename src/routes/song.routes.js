59894216d2ba38c1e061d62c71f6ceac46d5be9e
salaj07 <salaj9877@gmail.com>
2026-01-18 11:44:14 +0530

    logs

--- src/routes/song.routes.js
+++ src/routes/song.routes.js
@@ -9,8 +9,8 @@ const upload=multer({storage:multer.memoryStorage()})
 
 router.post('/songs',upload.single('audio'),  async (req,res)=>{
 
-    console.log(req.body);
-    console.log(req.file);
+    // console.log(req.body);
+    // console.log(req.file);
     const fileData= await uploadFile(req.file)
     
     

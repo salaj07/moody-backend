33b3e92b70d1195573e714a27e731a3b3040c923
salaj07 <salaj9877@gmail.com>
2026-01-18 11:49:29 +0530

    format

--- src/routes/song.routes.js
+++ src/routes/song.routes.js
@@ -1,51 +1,42 @@
-const express=require("express");
-const multer=require("multer");//form data format ko read krne ke liye
+const express = require("express");
+const multer = require("multer"); //use to read format of the data
 const uploadFile = require("../service/storage.service");
-const router=express.Router();
-const songModel=require("../models/song.model")
+const router = express.Router();
+const songModel = require("../models/song.model");
 
-const upload=multer({storage:multer.memoryStorage()})
+const upload = multer({ storage: multer.memoryStorage() });
 
+router.post("/songs", upload.single("audio"), async (req, res) => {
+  // console.log(req.body);
+  // console.log(req.file);
+  const fileData = await uploadFile(req.file);
 
-router.post('/songs',upload.single('audio'),  async (req,res)=>{
+  // console.log(fileData);
 
-    // console.log(req.body);
-    // console.log(req.file);
-    const fileData= await uploadFile(req.file)
-    
-    
-    console.log(fileData);
+  const song = await songModel.create({
+    title: req.body.title,
+    artist: req.body.artist,
+    audio: fileData.url,
+    mood: req.body.mood,
+  });
 
-    const song= await songModel.create({
-        title:req.body.title,
-        artist:req.body.artist,
-        audio:fileData.url,
-        mood:req.body.mood
-    })
-    
-    
-    
-    res.json({
-        
-        message:"song created",
-        song
-    })
-})
+  res.json({
+    message: "song created",
+    song,
+  });
+});
 
-router.get('/songs',async (req,res)=>{
+router.get("/songs", async (req, res) => {
+  const { mood } = req.query;
 
-    const {mood}=req.query;
+  const song = await songModel.find({
+    mood: mood,
+  });
 
-    const song =await songModel.find({
-        mood:mood
-    })
+  res.status(200).json({
+    message: "song fetched",
+    song,
+  });
+});
 
-    res.status(200).json({
-        message:"song fetched",
-        song
-    })
-    
-})
-
- 
-module.exports= router;
+module.exports = router;
